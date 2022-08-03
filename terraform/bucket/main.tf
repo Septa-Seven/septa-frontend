0df1e50f51afda89aaba90b31912f701b8411e1d
Шевела Роман Александрович <shevela@tochka.com>
2022-08-03 14:33:46 +0500

    feat(infrastructure): Create bucket and deploy site

---
+++ terraform/bucket/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.77.0"
+    }
+  }
+}
+
+resource "yandex_storage_bucket" "bucket" {
+  bucket = "septa-frontend"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
